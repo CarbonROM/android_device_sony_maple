9e45bab0b5f72e8cd541b01f92fe1e9d750e2246
Christian Oder <myself5@carbonrom.org>
2017-07-18 16:47:35 +0200

    maple: Carbonify
    Change-Id: I90a98d52c62b3b3bed89dc1386b6e38e33245c34

---
+++ carbon_maple.mk
@@ -0,0 +1,39 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Screen Resolution for the Bootanimation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit AOSP tone common device parts
+$(call inherit-product, device/sony/maple/aosp_g8141.mk)
+
+# Inherit Carbon tone common device parts
+$(call inherit-product, device/sony/yoshino-common/platform_carbon.mk)
+
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+# Override Product Name for CarbonROM
+PRODUCT_NAME := carbon_maple
+PRODUCT_MODEL := Xperia XZ Premium
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := G8141,maple
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="Myself5"
