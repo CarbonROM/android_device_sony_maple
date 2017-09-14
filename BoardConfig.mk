2a6542b57c69986fa11ee1a1038245ab680e2cb0
Christian Oder <myself5@carbonrom.org>
2017-09-14 23:44:10 +0200

    Allow overwriting values with Carbon configs
    Change-Id: I4e1876b1a6b47c57e0c82b4be18ac80f4a39a3ca

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/yoshino-common/PlatformConfigCarbon.mk
 include device/sony/yoshino-common/PlatformConfig.mk
+include device/sony/yoshino-common/PlatformConfigCarbon.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := G8141
 
