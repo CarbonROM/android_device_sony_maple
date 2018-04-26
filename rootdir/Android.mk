98da481cb638ef266f22f25d6ad941659739e53c
Christian Oder <myself5@carbonrom.org>
2018-04-26 15:18:31 +0200

    maple: Add FDE fstab to maple tree
    Change-Id: I11147f764152c411bc9254b5190acb0488072226

---
+++ rootdir/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.maple_fde
+LOCAL_SRC_FILES := vendor/etc/fstab.maple
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := fstab.maple
+LOCAL_MODULE_STEM := fstab.maple_fde
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc
+include $(BUILD_PREBUILT)
