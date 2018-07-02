0737074f264565bbac5263b13c125f13acdbea6e
Christian Oder <myself5@carbonrom.org>
2018-07-02 13:36:15 +0200

    Unify maple devices variants
    * Credits to Luk1337 for the device detection method
    
    Change-Id: Ifbbd934197a569611a6ab45de5332c25502ef6a8

--- carbon_maple.mk
+++ carbon_maple.mk
@@ -20,13 +20,7 @@ $(call inherit-product, vendor/carbon/config/common.mk)
 PRODUCT_NAME := carbon_maple
 PRODUCT_DEVICE := maple
 PRODUCT_BRAND := Sony
-PRODUCT_MODEL := G8141
 PRODUCT_MANUFACTURER := Sony
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=maple
-
-BUILD_FINGERPRINT:=Sony/maple/maple:8.0.0/OPR6.170623.013/1:user/dev-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carbon.maintainer="Myself5"
