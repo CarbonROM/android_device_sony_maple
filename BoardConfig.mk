c5d14e51fdc11c2f4fbc9cadc96aac089ca09400
Christian Oder <myself5@carbonrom.org>
2018-09-13 22:33:59 +0200

    maple: extend yoshino init with maple init
    Change-Id: Idd1c6830afbf3f116ed8a6ff45d3dc94358d2e48

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ DEVICE_PATH := device/sony/maple
 PRODUCT_PLATFORM := yoshino
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_maple
+TARGET_LIBINIT_YOSHINO_DEFINES_FILE := $(DEVICE_PATH)/init_maple/init_maple.cpp
 TARGET_UNIFIED_DEVICE := true
 
 ### BOOTLOADER
