449f75b7378eaa7403546b551e44e25d231cd80d
Christian Oder <myself5@carbonrom.org>
2019-08-24 18:11:13 +0200

    maple: build libandroid_net
    * Fixes Mobile data
    
    Change-Id: I1b694badfa6878935f5719fdda413be301caa656

--- device/hardware.mk
+++ device/hardware.mk
@@ -11,3 +11,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
+
+# RIL
+PRODUCT_PACKAGES += \
+    libandroid_net
