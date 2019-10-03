3aa9b4b1e51302cfbb47e5ff537d042eb44f27e0
Sujin Panicker <spanic@codeaurora.org>
2019-10-03 12:16:58 +0200

    maple: audio: Rename audio_platform_info to audio_platform_info_intcodec
    * fixes Audio with new CAF Audio Hals
    * refering to https://github.com/LineageOS/android_hardware_qcom_audio/commit/b904fbec4986fb371c81cf550f2615052874bf40
    
    Change-Id: I9adc4cffa8b3b9d0361d340ce062527425e0d4fa

--- device/config-files.mk
+++ device/config-files.mk
@@ -7,4 +7,4 @@ PRODUCT_COPY_FILES += \
 ### Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
-    $(DEVICE_PATH)/config/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+    $(DEVICE_PATH)/config/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml
